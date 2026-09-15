05ff5f245f2309b0a204c0d4f3ad34571939520f
Natalia0704 <a388413@uach.mx>
2026-09-15 09:41:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.title("Clasificador de temperatura")
 temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20)
-if a>=20:
+if temperatura>=20:
     print("hace calor") 
-elif a<=20:
+elif temperatura<=20:
     print("hace frio") 
 else:
     print("agradable")
