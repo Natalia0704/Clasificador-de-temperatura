c14baca6c29a3b89ccb18fa5113870fa9982f0b8
Natalia0704 <a388413@uach.mx>
2026-09-15 09:43:43 -0600

    Fix temperature comparison logic in app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.title("Clasificador de temperatura")
 temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20)
-if temperatura>=20:
+if temperatura>20:
     st.write("hace calor") 
-elif temperatura<=20:
+elif temperatura<20:
     st.write("hace frio") 
 else:
     st.write("agradable")
