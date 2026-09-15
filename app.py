46e1ed40e856f122ba980cffaed59fd7dacd15ec
Natalia0704 <a388413@uach.mx>
2026-09-15 09:37:28 -0600

    Correct temperature conditionals in app.py
    Fix conditional statements for temperature evaluation.

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20)
 if a>=20: print("hace calor") 
-elif a<=20: print("hace frio") 
-else a==20: print("agradable")
+elif: a<=20: print("hace frio") 
+else: a==20: print("agradable")
