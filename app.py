ce1c8afc6f0b6f53f0d47b9c97ba3aa3ea212503
Natalia0704 <a388413@uach.mx>
2026-09-15 09:42:42 -0600

    Replace print statements with st.write for output

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20)
 if temperatura>=20:
-    print("hace calor") 
+    st.write("hace calor") 
 elif temperatura<=20:
-    print("hace frio") 
+    st.write("hace frio") 
 else:
-    print("agradable")
+    st.write("agradable")
