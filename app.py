9847092eb433f1f003efdcfa0c901cbb651b74c6
Natalia0704 <a388413@uach.mx>
2026-09-15 09:35:25 -0600

    Fix conditional statements for temperature output

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ st.title("Clasificador de temperatura")
 temperatura = st.number_input(
     "Introduce la temperatura en °C:",
     value=20)
-if a>=20: print("hace calor") elif a<=20: print("hace frio") else a==20 print("agradable")
+if a>=20: print("hace calor") 
+elif a<=20: print("hace frio") 
+else a==20 print("agradable")
